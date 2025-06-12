240885003ecb482a9c017d23ee1e5309d3accac2
Thibault de Saint Sernin <thibault@dfns.co>
2025-06-12 11:58:58 +0200

    fix: deprecate apps

--- app/src/main/java/co/dfns/sdk/tutorial/mobile/Constants.kt
+++ app/src/main/java/co/dfns/sdk/tutorial/mobile/Constants.kt
@@ -1,7 +1,6 @@
 package co.dfns.sdk.tutorial.mobile
 
 object Constants {
-    const val DFNS_APP_ID = "ap-1l868-h1p4t-97l8p4lorctgb861"
     const val SERVER_BASE_URL = "https://d0d7-31-217-63-194.ngrok-free.app"
     const val PASSKEY_RELYING_PARTY_ID = "ngrok-free.app"
     const val PASSKEY_RELYING_PARTY_NAME = "Demo App"
